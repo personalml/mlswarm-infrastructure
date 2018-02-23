09d74c7b28befa4917ee47d8637b5a2e24b145ff
ldavid <lucas.david@solvian.com>
2018-02-23 11:08:12 -0300

    clear trainers, making them part of the estimator itself

--- mlswarm/infrastructure/services/estimators/serializers.py
+++ mlswarm/infrastructure/services/estimators/serializers.py
@@ -11,6 +11,23 @@ class IEstimator(ServiceSerializerMixin,
 
 
 class SimpleDenseNetworkClassifier(IEstimator):
+    class Train(Serializer):
+        learning_rate = serializers.FloatField(
+            min_value=0,
+            default=0.01,
+            help_text='Learning rate used by the optimizer when adjusting '
+                      'weights.')
+        optimizer = serializers.CharField(
+            max_length=32,
+            default='adam',
+            help_text='Optimizer used to train the network.')
+        dropout = serializers.FloatField(
+            min_value=0,
+            max_value=1,
+            default=0,
+            help_text='Dropout probability rate used in the dropout layers. '
+                      '0 means no dropout at all.')
+
     input_units = serializers.IntegerField(min_value=1, required=True)
     inner_units = serializers.IntegerField(min_value=1, required=True)
     output_units = serializers.IntegerField(min_value=1, required=True)
@@ -21,6 +38,9 @@ class SimpleDenseNetworkClassifier(IEstimator):
 
 
 class SimpleRegressor(IEstimator):
+    class Train(Serializer):
+        max_iterations = serializers.IntegerField(min_value=-1)
+
     input_units = serializers.IntegerField(min_value=1, required=True)
     output_units = serializers.IntegerField(min_value=1, required=True)
 
--- setup.py
+++ setup.py
@@ -7,15 +7,16 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(base_dir, 'requirements.txt')) as f:
     base_requirements = f.readlines()
 
-setup(name='mlswarm_api-infrastructure',
+setup(name='mlswarm-infrastructure',
       description='Basic infrastructure necessary for ML Swarm usage.',
       keywords=['machine-learning'],
       version='0.1',
-      packages=['mlswarm.infrastructure',
-                'mlswarm.infrastructure.services',
-                'mlswarm.infrastructure.services.estimators',
-                'mlswarm.infrastructure.services.trainer',
-                'mlswarm.infrastructure.services.parsers'],
+      packages=[
+          'mlswarm.infrastructure',
+          'mlswarm.infrastructure.services',
+          'mlswarm.infrastructure.services.estimators',
+          'mlswarm.infrastructure.services.parsers'
+      ],
       scripts=[],
 
       author='Lucas David',
